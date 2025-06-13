3b2ea7d0cf0b5dece1c3fff1a74d39a492af29df
vict0245 <vf1838829@gmail.com>
2025-06-12 22:50:05 -0500

    subiendo parte del metodos-administrador

--- Backend/proyectoWeb/src/main/java/com/example/demo/controlador/AdministradoresControlador.java
+++ Backend/proyectoWeb/src/main/java/com/example/demo/controlador/AdministradoresControlador.java
@@ -1,5 +1,46 @@
 package com.example.demo.controlador;
 
-public class AdministradoresControlador {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.modelo.Administradores;
+import com.example.demo.repositorio.AdministradoresRepositorio;
+
+
+@RestController
+@RequestMapping("/administradores")
+public class AdministradoresControlador {
+	@Autowired
+	private AdministradoresRepositorio repositorioA;
+	
+	@PostMapping("/guardar")
+	public Object guardarA(@RequestBody Administradores a) {
+		try {
+			Administradores nuevoAdministrador = this.repositorioA.save(a);	
+			return ResponseEntity.status(HttpStatus.CREATED).body(nuevoAdministrador);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(null);
+		}
+	}
+	
+	@PostMapping("/iniciar")
+	public Object iniciarSecionA(@RequestParam String id,@RequestParam String password) {
+		String Apass=this.repositorioA.buscarAdministrador(0);
+		if(Apass.isEmpty()) {
+			return "no hay nada jajaja";
+		}
+		else if(Apass.equals(password)) {
+			return "yes";
+		}else {
+			return "no";
+		}
+			
+	}
 }
--- Backend/proyectoWeb/src/main/java/com/example/demo/repositorio/AdministradoresRepositorio.java
+++ Backend/proyectoWeb/src/main/java/com/example/demo/repositorio/AdministradoresRepositorio.java
@@ -1,5 +1,16 @@
 package com.example.demo.repositorio;
 
-public interface AdministradoresRepositorio {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import com.example.demo.modelo.Administradores;
+
+@Repository
+public interface AdministradoresRepositorio extends JpaRepository<Administradores,Long> {
+	
+	
+	@Query(value="select password from administradores where id_administrador=:id",nativeQuery=true)
+	public String buscarAdministrador(@Param("id")int id);
 }
