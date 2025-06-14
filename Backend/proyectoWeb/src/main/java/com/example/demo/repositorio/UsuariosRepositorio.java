8469a7720a612051ea84f9530c212a8eb13aa507
juan1705M <davidma1705m@gmail.com>
2025-06-14 00:59:24 -0500

    Guardar el registro de usuario en el backend

--- Backend/proyectoWeb/src/main/java/com/example/demo/repositorio/UsuariosRepositorio.java
+++ Backend/proyectoWeb/src/main/java/com/example/demo/repositorio/UsuariosRepositorio.java
@@ -1,5 +1,21 @@
 package com.example.demo.repositorio;
 
-public interface UsuariosRepositorio {
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.modelo.Usuarios;
+
+public interface UsuariosRepositorio extends JpaRepository<Usuarios, Integer>{
+	
+	public Usuarios findByIdUsuario(int idUsuario);
+	public List<Usuarios> findByNombre(String nombre);
+	public List<Usuarios> findByFechaExpedicion(LocalDate fechaExpedicion);
+	public List<Usuarios> findByCategoria(String categoria);
+	public List<Usuarios> findByVigencia(LocalDate vigencia);
+	public Usuarios findByEmail(String email);
+	public List<Usuarios> findByTelefono(String telefono);
 
 }
