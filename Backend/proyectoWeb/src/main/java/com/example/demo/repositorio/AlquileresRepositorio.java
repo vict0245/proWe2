c8c0dbd431fe8d1ee8c4393732844f32b4e138b4
vict0245 <105755508+vict0245@users.noreply.github.com>
2025-06-17 17:30:58 -0500

    Revert "Subiendo Progreso"

--- Backend/proyectoWeb/src/main/java/com/example/demo/repositorio/AlquileresRepositorio.java
+++ Backend/proyectoWeb/src/main/java/com/example/demo/repositorio/AlquileresRepositorio.java
@@ -1,33 +1,5 @@
 package com.example.demo.repositorio;
 
-import java.util.List;
+public interface AlquileresRepositorio {
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import java.util.Date;
-
-import com.example.demo.modelo.Alquileres;
-
-public interface AlquileresRepositorio extends JpaRepository<Alquileres,Long>{
-	
-//El sistema muestra un catálogo de vehículos
-//disponibles, incluyendo detalles como modelo, 
-//tipo, precio por día.
-	
-	@Query(value=" select modelo,tipo,valor_alquiler_dia from vehiculos",nativeQuery=true)
-	public List<Object[]> mostrarVehi();
-	
-//El sistema calcula el costo total del alquiler basándose en el vehículo y el período.
-	
-	@Query(value="select valor_total from alquileres v where v.id_vehiculo =: id and v.fecha_inicio =: FI and v.fecha_fin =: FF",nativeQuery=true)
-	public List<Object> valorTotal(@Param("id")int id_vehiculo,@Param("FI")Date fecha_inicio,@Param("FF")Date fecha_fin);
-	
-// El sistema presenta al cliente un resumen de la solicitud, incluyendo el vehículo, las fechas, el costo.
-	
-	@Query(value="select id_vehiculo,fecha_inicio,fecha_fin,valor_total from alquileres",nativeQuery=true)
-	public List<Object[]> soli();
-			
-
-	
 }
