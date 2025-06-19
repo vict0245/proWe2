f97605f54cf4804129000ce07a91440a6a4cd1f2
vict0245 <vf1838829@gmail.com>
2025-06-18 21:34:28 -0500

    poniendo crossOrigin a los controladores

--- Backend/proyectoWeb/src/main/java/com/example/demo/controlador/AdministradoresControlador.java
+++ Backend/proyectoWeb/src/main/java/com/example/demo/controlador/AdministradoresControlador.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import com.example.demo.repositorio.VehiculosRepositorio;
 
 @RestController
 @RequestMapping("/administradores")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class AdministradoresControlador {
 	
 	@Autowired
--- Backend/proyectoWeb/src/main/java/com/example/demo/controlador/Gestion_AlquilerControlador.java
+++ Backend/proyectoWeb/src/main/java/com/example/demo/controlador/Gestion_AlquilerControlador.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,6 +29,7 @@ import com.example.demo.repositorio.VehiculosRepositorio;
 
 @RestController
 @RequestMapping("/gestionAlquiler")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class Gestion_AlquilerControlador {
 	
 	@Autowired
